5e1248d9cb09c14f511c30dbb7944bfbff298bce
Chris Steinke <chrisjsteinke@gmail.com>
2021-12-29 19:59:23 -0800

    Added chapter 1 exercise 21

--- exercises/ch1/exercise1-21.c
+++ exercises/ch1/exercise1-21.c
@@ -6,45 +6,33 @@
 int mygetline(char line[], int maxline);
 void findspaces(char line[]);
 
-/* Replaces strings of blanks by the minimum number of tabls to space to the 
- * next tab stop
+
+/*
+ * A program 'entab' that replaces strings of blanks by the minimum
+ * number of tabs and blanks to achieve the same spacing. Use the same tab stops as for detab.
+
  */
 
 int main() 
 {
 
 	int i, c, sc = 0, j, k; 
-	int need_tabs;
-
+	
 	for (i = 1; (c = getchar()) != EOF; i++) {
 		if ( c == ' ') {
-			++sc;
-		} else if (c == '\t') {
-			printf("\t");
-			if (sc > 0 && sc < TABSTOP) {
-				for (j = 1; j <= sc; j++) {
-					printf(" ");
-				}
-				sc = 0;
-			} else if (sc == TABSTOP) {
-				printf("\t");
-				sc = 0;
-			}
+		  ++sc;
+		  if (sc >= TABSTOP) {
+		    printf("\t");
+		  } else {
+		    printf(" ");
+		    sc = 0;
+		  }
 		} else {
-			/* We have a regular character */
-			if (sc > 0 && sc < TABSTOP) {
-				for (k = 1; k <= sc; k++) {
-					printf(" ");
-				}
-				sc = 0;
-			} else if  (sc == TABSTOP) {
-				printf("\t");
-				sc = 0;
-			}
-			putchar(c);
-			sc = 0;
+		  putchar(c);
+		  sc = 0; 
 		}
 	}
+	
 }
 
 
